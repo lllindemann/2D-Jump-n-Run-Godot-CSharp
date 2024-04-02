0ac5d15dc67536da8a5a3eeb179eda14a04a3c36
llindemann <leonie.lindemann.ll@gmail.com>
2024-04-02 20:25:53 +0200

    reafactor: C# naming conventions in #PlayerController

--- scenes/player/PlayerController.cs
+++ scenes/player/PlayerController.cs
@@ -1,7 +1,10 @@
 using Godot;
 using System;
 
-public partial class player_controller : CharacterBody2D
+/// <summary>
+/// Inherit from this base class to create a singleton class which has access on monobehavior functions.
+/// </summary>
+public partial class PlayerController : CharacterBody2D
 {
 	// variables relevant for main player movement
 	[Export]
@@ -15,18 +18,18 @@ public partial class player_controller : CharacterBody2D
 	[Export]
 	public float JUMP_VELOCITY = 400.0f;
 
-	public float gravity = ProjectSettings.GetSetting("physics/2d/default_gravity").AsSingle();
+	public float Gravity = ProjectSettings.GetSetting("physics/2d/default_gravity").AsSingle();
 
 
-	Vector2 velocity = new Vector2();
-	bool jumping = false;
+	private Vector2 _velocity = new Vector2();
+	private bool _jumping = false;
 
-	AnimatedSprite2D _sprite2d;
+	private AnimatedSprite2D _Sprite2d;
 
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
 	{
-		_sprite2d = GetNode<AnimatedSprite2D>("AnimatedSprite2D");
+		_Sprite2d = GetNode<AnimatedSprite2D>("AnimatedSprite2D");
 		SetupAnimation();
 	}
 
@@ -39,47 +42,46 @@ public partial class player_controller : CharacterBody2D
 	// Called every frame. 'delta' is the elapsed time since the previous frame.
 	public override void _PhysicsProcess(double delta)
 	{
-		velocity = Velocity;
+		_velocity = Velocity;
 		getInput();
 
 		// add gravity on player when he is in the air
 		if (!IsOnFloor())
 		{
-			velocity.Y += gravity * (float)delta;
+			_velocity.Y += Gravity * (float)delta;
 
 		}
 
-		Velocity = velocity;
+		Velocity = _velocity;
 		MoveAndSlide();
 	}
 
+
 	public void getInput()
 	{
-
-		velocity.X = 0;
+		_velocity.X = 0;
 
 		// We check for each move input and update the direction accordingly.
 		if (Input.IsKeyPressed(Key.Right) || Input.IsKeyPressed(Key.D))
 		{
-			velocity.X += MOVE_SPEED;
+			_velocity.X += MOVE_SPEED;
 
 		}
 		if (Input.IsKeyPressed(Key.Left) || Input.IsKeyPressed(Key.D))
 		{
-			velocity.X -= MOVE_SPEED;
+			_velocity.X -= MOVE_SPEED;
 		}
 		if ((Input.IsKeyPressed(Key.Space) || Input.IsKeyPressed(Key.Up)) && IsOnFloor())
 		{
-			jumping = true;
-			velocity.Y = -JUMP_VELOCITY;
+			_jumping = true;
+			_velocity.Y = -JUMP_VELOCITY;
 		}
 	}
-
 	public void SetupAnimation()
 	{
-		_sprite2d.Animation = "default";
-		_sprite2d.Play();
-		_sprite2d.SpeedScale = 1;
+		_Sprite2d.Animation = "default";
+		_Sprite2d.Play();
+		_Sprite2d.SpeedScale = 1;
 
 
 	}
@@ -87,27 +89,27 @@ public partial class player_controller : CharacterBody2D
 	public void ProcessAnimation()
 	{
 
-		if (Math.Abs(velocity.X) >= 20)
+		if (Math.Abs(_velocity.X) >= 20)
 		{
-			_sprite2d.FlipH = false;
+			_Sprite2d.FlipH = false;
 
-			if (velocity.X < 0)
+			if (_velocity.X < 0)
 			{
-				_sprite2d.FlipH = true;
+				_Sprite2d.FlipH = true;
 			}
 		}
 
 		if (!IsOnFloor())
 		{
-			_sprite2d.Play("fall", 1);
+			_Sprite2d.Play("fall", 1);
 		}
-		else if (Math.Abs(velocity.X) >= 20)
+		else if (Math.Abs(_velocity.X) >= 20)
 		{
-			_sprite2d.Play("run", 3);
+			_Sprite2d.Play("run", 3);
 		}
 		else
 		{
-			_sprite2d.Play("default", 1);
+			_Sprite2d.Play("default", 1);
 		}
 
 
