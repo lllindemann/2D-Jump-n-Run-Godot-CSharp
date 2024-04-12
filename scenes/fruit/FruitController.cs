acc191328174c37ea2bc35f59017802bedc884d7
llindemann <leonie.lindemann.ll@gmail.com>
2024-04-12 21:50:27 +0200

    feat: hide fruits when body entered by player

--- scenes/fruit/FruitController.cs
+++ scenes/fruit/FruitController.cs
@@ -7,13 +7,14 @@ public partial class FruitController : Area2D
 	public SpriteFrames[] SPRITES { get; set; } = { };
 
 	private AnimatedSprite2D _Sprite2d;
+	private bool _active = true;
 
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
 	{
 		_Sprite2d = GetNode<AnimatedSprite2D>("AnimatedSprite2D");
 		var rand = new Random();
-		_Sprite2d.SpriteFrames = SPRITES[rand.Next(0, SPRITES.Length -1 )];
+		_Sprite2d.SpriteFrames = SPRITES[rand.Next(0, SPRITES.Length - 1)];
 		_Sprite2d.Play();
 	}
 
@@ -21,5 +22,23 @@ public partial class FruitController : Area2D
 	public override void _Process(double delta)
 	{
 
+
+
+	}
+
+	private void _on_fruit_body_entered(Node2D body)
+	{
+		// Check if entrering object is player node
+		if (body.Name == "Player")
+		{
+			_active = false;
+			Hide();
+		}
+	}
+
+	private void Reset()
+	{
+		_active = true;
+		Show();
 	}
 }
